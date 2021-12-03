3120b3dfc696e060ddecdba5c10cdc64f719d821
Casppiii <95220445+Casppiii@users.noreply.github.com>
2021-12-03 10:59:37 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,20 +2,23 @@
 void Main()
 {
     string førsteSpørgsmål = "Er ballondor rigged?";
-    
+
     while (true)
     {
-    Console.WriteLine("Velkommen til quiz game");
-    Console.WriteLine("Spørgsmål " + førsteSpørgsmål);
-    string svar = Console.ReadLine();
-    tjekSvarFørsteSpørgsmål(svar);
-    Andetspørgesmål();
-    svar = Console.ReadLine();
-    tjekandetspørgesmål(svar);
-    Tredjespørgesmål();
-    svar =Console.ReadLine();
-    tjektredjespørgesmål(svar);
-
+        diller();
+        Console.WriteLine("Velkommen til quiz game");
+        Console.WriteLine("Spørgsmål " + førsteSpørgsmål);
+        string svar = Console.ReadLine();
+        tjekSvarFørsteSpørgsmål(svar);
+        Andetspørgesmål();
+        svar = Console.ReadLine();
+        tjekandetspørgesmål(svar);
+        Tredjespørgesmål();
+        svar = Console.ReadLine();
+        tjektredjespørgesmål(svar);
+        fjærdespørgesmål();
+        svar= Console.ReadLine();
+        tjekfjærdespørgesmål(svar);
 
     }
 
@@ -28,14 +31,14 @@ void tjekSvarFørsteSpørgsmål(string svaret)
     string førsteSpørgsmål = "Er ballondor rigged?";
 
     bool fuck = true;
-    if(svaret == "Ja")
+    if (svaret == "Ja")
     {
         Console.WriteLine("korrekt");
     }
-    else 
+    else
     {
         Console.WriteLine("du tar fejl");
-        
+
         while (fuck)
         {
             Console.WriteLine("Spørgsmål " + førsteSpørgsmål);
@@ -48,13 +51,13 @@ void tjekSvarFørsteSpørgsmål(string svaret)
         }
 
     }
-    
+
 }
 void Andetspørgesmål()
 {
     string Andetspørgesmål = "hvor mange søer er der i viborg";
     Console.WriteLine(Andetspørgesmål);
-    
+
 }
 void tjekandetspørgesmål(string svaret)
 {
@@ -69,14 +72,14 @@ void tjekandetspørgesmål(string svaret)
 
     }
 }
-void Tredjespørgesmål ()
-{ 
+void Tredjespørgesmål()
+{
     string tredjespørgesmål = "hvad er adressen til mercantec hca";
     Console.WriteLine(tredjespørgesmål);
 }
 void tjektredjespørgesmål(string svaret)
 {
-    if (svaret == "hcandersensvej 7") 
+    if (svaret == "hcandersensvej 7")
     {
         Console.WriteLine("korrekt");
     }
@@ -85,7 +88,27 @@ void tjektredjespørgesmål(string svaret)
         Console.WriteLine("du så dum at du nok også har svarret forkert på de to andre");
     }
 }
+void fjærdespørgesmål ()
+{
+    string fjærdespørgesmål = "hvad er navnet for by delen som starter med h";
+    Console.WriteLine(fjærdespørgesmål);
+}
+void tjekfjærdespørgesmål(string svaret)
+{
+    if (svaret == "houlkjær")
+    {
+        Console.WriteLine("korrekt");
+    }
+    else
+    {
+        Console.WriteLine( "tag dig sammen og tænk dig om");
+    }
 
-
-
+}
+void diller ()
+{
+    String[] pølse
+        = new String[] { "hej", "med", "dig" };
+    for (int i = 0; i < pølse.Length; i++) { Console.WriteLine(pølse[i]); }
+}
 Main();
